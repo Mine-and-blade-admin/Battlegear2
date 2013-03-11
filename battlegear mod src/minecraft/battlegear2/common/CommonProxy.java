6878ebdfd02865728363ba543b1c68c9041ed47a
GotoLink <olivier.sylvain@gmail.com>
2013-03-11 22:36:30 +0100

    Update CommonProxy.java
    Pushed events into proxies

--- battlegear mod src/minecraft/battlegear2/common/CommonProxy.java
+++ battlegear mod src/minecraft/battlegear2/common/CommonProxy.java
@@ -3,5 +3,16 @@ package battlegear2.common;
 public class CommonProxy {
 	
 	public void registerKeyHandelers(){}
-
+	@ForgeSubscribe
+	public void playerInterect(PlayerInteractEvent event){
+	}
+	
+	@ForgeSubscribe
+	public void playerIntereactEntity(EntityInteractEvent event){
+		if(event.entityPlayer.inventory.isBattlemode()){
+			event.entityPlayer.swingOffItem();
+			event.entityPlayer.attackTargetEntityWithCurrentOffItem(event.target);
+			event.setCanceled(true);
+		}
+	}
 }
