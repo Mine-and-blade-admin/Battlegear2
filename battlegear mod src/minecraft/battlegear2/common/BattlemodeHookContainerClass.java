d0be3ac51e2322cc71f4620549038b054c29d1f9
GotoLink <olivier.sylvain@gmail.com>
2013-03-11 22:35:32 +0100

    Update BattlemodeHookContainerClass.java
    Pushed events into proxies.

--- battlegear mod src/minecraft/battlegear2/common/BattlemodeHookContainerClass.java
+++ battlegear mod src/minecraft/battlegear2/common/BattlemodeHookContainerClass.java
@@ -18,43 +18,6 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 
 public class BattlemodeHookContainerClass {
 
-	@ForgeSubscribe
-	public void playerInterect(PlayerInteractEvent event){
-		if(FMLCommonHandler.instance().getSide() == Side.CLIENT){//we could put this into client proxy instead of checking the side
-			
-			if(event.entityPlayer.inventory.isBattlemode()){
-				Minecraft mc = FMLClientHandler.instance().getClient();
-				switch (event.action) {
-				case LEFT_CLICK_BLOCK:
-					System.out.println("clicked");
-					break;
-				case RIGHT_CLICK_BLOCK:
-					//TODO add support for tools, although I think this could
-					//require too many edits and is likely not viable
-					event.entityPlayer.swingOffItem();
-					event.useItem = Result.DENY;
-					break;
-				case RIGHT_CLICK_AIR:
-					ItemStack mainHand = event.entityPlayer.getCurrentEquippedItem();
-					if(mainHand == null || BattlegearUtils.isMainHand(mainHand.itemID)){
-						event.entityPlayer.swingOffItem();
-						event.setCanceled(true);
-						break;
-					}else{
-							break;
-					}
-				}
-			}
-		}
-	}
 	
-	@ForgeSubscribe
-	public void playerIntereactEntity(EntityInteractEvent event){
-		if(event.entityPlayer.inventory.isBattlemode()){
-			event.entityPlayer.swingOffItem();
-			event.entityPlayer.attackTargetEntityWithCurrentOffItem(event.target);
-			event.setCanceled(true);
-		}
-	}
 	
 }
