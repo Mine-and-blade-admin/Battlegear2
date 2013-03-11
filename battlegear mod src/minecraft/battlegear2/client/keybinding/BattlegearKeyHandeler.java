5c136847d83fe6c6baac2af7c2bfdc500c031e2b
GotoLink <olivier.sylvain@gmail.com>
2013-03-11 22:05:20 +0100

    Update BattlegearKeyHandeler.java
    Fixes and ideas

--- battlegear mod src/minecraft/battlegear2/client/keybinding/BattlegearKeyHandeler.java
+++ battlegear mod src/minecraft/battlegear2/client/keybinding/BattlegearKeyHandeler.java
@@ -30,20 +30,23 @@ public class BattlegearKeyHandeler extends KeyHandler{
 	@Override
 	public void keyDown(EnumSet<TickType> types, KeyBinding kb,
 			boolean tickEnd, boolean isRepeat) {
-		
-		if(FMLClientHandler.instance().getClient().currentScreen == null){
+		Minecraft client=FMLClientHandler.instance().getClient();
+		if(client!=null && client.thePlayer!=null && client.currentScreen == null){//null checks to prevent any crash outside the world
 			if(kb.keyCode == battleInv.keyCode){
-				 FMLClientHandler.instance().getClient().thePlayer.swingOffItem();
+				 client.thePlayer.swingOffItem();
+				 //can send a packet here instead of inside the upper method
+				 //i'd go with a: commonproxy.sendPacket(client.thePlayer,new Packet18Animation(client.thePlayer, 1));
+				 //then the proxy would decide what to do depending on the effective side and type of entityplayer
 			}else if (kb.keyCode == drawWeapons.keyCode && tickEnd){
-				InventoryPlayer playerInventory = FMLClientHandler.instance().getClient().thePlayer.inventory;
+				InventoryPlayer playerInventory = client.thePlayer.inventory;
 				if(playerInventory.isBattlemode()){
 					previousBattlemode = playerInventory.currentItem;
 					playerInventory.currentItem = previousNormal;
-				}else{
+				}else{  //i'd use int bounds check (0-8) for the item, just in case
 					previousNormal = playerInventory.currentItem;
 					playerInventory.currentItem = previousBattlemode;
 				}
-				FMLClientHandler.instance().getClient().playerController.updateController();
+				client.playerController.updateController();
 			}
 		}
 	}
