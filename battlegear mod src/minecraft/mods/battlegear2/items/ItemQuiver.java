85c67cd67afa04ec84be10cd540b39e64c02ba7a
GotoLink <gotolinkminecraft@gmail.com>
2013-08-20 01:09:04 +0200

    Changed arrow count to NBT in quiver

--- battlegear mod src/minecraft/mods/battlegear2/items/ItemQuiver.java
+++ battlegear mod src/minecraft/mods/battlegear2/items/ItemQuiver.java
@@ -1,25 +1,33 @@
 package mods.battlegear2.items;
 
+import java.util.List;
+
 import mods.battlegear2.api.IArrowContainer;
 import mods.battlegear2.api.QuiverArrowEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 public class ItemQuiver extends Item implements IArrowContainer{
 
+	public static final String TAG = "Arrows";
+	public static int maxArrow = 256;
+	//This serves as the arrow limit, more than 64 to provide an advantage over a simple arrow stack
 	public ItemQuiver(int par1) {
 		super(par1);
 		this.setMaxStackSize(1);
-		this.setMaxDamage(128);
-		//This serves as the arrow count, more than 64 to provide an advantage over a simple arrow stack
 	}
 
 	@Override
-	public boolean hasArrow(ItemStack stack) {
-		return stack.getItemDamage()<this.getMaxDamage();
+	public boolean hasArrowFor(ItemStack stack, ItemStack bow, EntityPlayer player) {
+		if(stack.hasTagCompound())
+			return stack.getTagCompound().getInteger(TAG)>0;
+		else
+			return false;
 	}
 
 	@Override
@@ -29,11 +37,54 @@ public class ItemQuiver extends Item implements IArrowContainer{
 
 	@Override
 	public void onArrowFired(World world, EntityPlayer player, ItemStack stack, ItemStack bow, EntityArrow arrow) {
-		stack.setItemDamage(stack.getItemDamage()+1);
+		stack.getTagCompound().setInteger(TAG, stack.getTagCompound().getInteger(TAG)-1);
 	}
 
 	@Override
 	public void onPreArrowFired(QuiverArrowEvent arrowEvent) {	
 	}
 
+	@Override
+	public boolean isCraftableWithArrows(ItemStack stack) {
+		return true;
+	}
+	
+	@Override
+	public int addArrows(ItemStack quiver, int arrows) {
+		int drop;
+		if(quiver.hasTagCompound())
+		{
+			drop = quiver.getTagCompound().getInteger(TAG)+arrows-maxArrow; 
+			if(drop>=0)
+				quiver.getTagCompound().setInteger(TAG,maxArrow);
+			else
+				quiver.getTagCompound().setInteger(TAG,drop+maxArrow);
+		}
+		else
+		{
+			drop = arrows-maxArrow;
+			NBTTagCompound tag = new NBTTagCompound();
+			if(drop>=0)
+				tag.setInteger(TAG, maxArrow);
+			else
+				tag.setInteger(TAG, arrows);
+			quiver.setTagCompound(tag);
+		}
+		return drop>0?drop:0;
+	}
+
+	@Override
+    public void addInformation(ItemStack stack, EntityPlayer par2EntityPlayer, List list, boolean par4) {
+        super.addInformation(stack, par2EntityPlayer, list, par4);
+        if(stack.hasTagCompound())
+        {
+        	int arrow = stack.getTagCompound().getInteger(TAG);
+        	if(arrow>0)
+        	{
+        		list.add(arrow+StatCollector.translateToLocal(" attribute.name.quiver.arrow.count"));
+        		return;
+        	}
+        }
+        list.add(StatCollector.translateToLocal("attribute.name.quiver.empty"));
+	}
 }
