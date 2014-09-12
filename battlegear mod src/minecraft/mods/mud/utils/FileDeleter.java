50e560b9fc2ccc3d60f4e9ab5a5f3f720b1515be
GotoLink <gotolinkminecraft@gmail.com>
2014-09-12 21:24:50 +0200

    Remove internal class loading

--- battlegear mod src/minecraft/mods/mud/utils/FileDeleter.java
+++ battlegear mod src/minecraft/mods/mud/utils/FileDeleter.java
@@ -1,22 +1,17 @@
 package mods.mud.utils;
 
-import mods.mud.ModUpdateDetector;
-
 import java.io.File;
 
 public class FileDeleter {
     public static void main(String[] args){
         if(args.length > 0){
             File f = new File(args[0]);
-            if(f.exists()){
-                while(f.exists()){
-                    f.delete();
-                    ModUpdateDetector.logger.trace("File in use, try again in 1 second");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            while(f.exists()){
+                if(!f.delete())
+                    System.out.println("File in use, try again in 1 second");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {
                 }
             }
 
