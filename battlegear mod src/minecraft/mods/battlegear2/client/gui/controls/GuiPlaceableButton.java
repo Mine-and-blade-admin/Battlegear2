e8f4c52d87c6bb9ffae406532c172e35e637261f
GotoLink <gotolinkminecraft@gmail.com>
2014-10-21 14:04:33 +0200

    Move buttons out of potion effects icons
    -Allow swapping back into main inventory

--- battlegear mod src/minecraft/mods/battlegear2/client/gui/controls/GuiPlaceableButton.java
+++ battlegear mod src/minecraft/mods/battlegear2/client/gui/controls/GuiPlaceableButton.java
@@ -1,13 +1,20 @@
 package mods.battlegear2.client.gui.controls;
 
+import mods.battlegear2.Battlegear;
+import mods.battlegear2.gui.BattlegearGUIHandeler;
+import mods.battlegear2.packet.BattlegearGUIPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiInventory;
 
 public abstract class GuiPlaceableButton extends GuiButton {
 	public static final int HEIGHT = 20;
+    private int deltaX = 0;
+    private final String oldName;
 	public GuiPlaceableButton(int par1, int par2, int par3, String name) {
 		super(par1, par2, par3, HEIGHT+4, HEIGHT, name);
+        oldName = name;
 	}
 
 	public void place(int count, int guiLeft, int guiTop) {
@@ -17,24 +24,38 @@ public abstract class GuiPlaceableButton extends GuiButton {
 
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
+        this.xPosition -= deltaX;
 		boolean inWindow = super.mousePressed(mc, mouseX, mouseY);
-		if (inWindow && !isInGui(mc.currentScreen)) {
-			this.openGui(mc);
-		}
+        this.xPosition += deltaX;
+		if (inWindow){
+            if(!isInGui(mc.currentScreen))
+                this.openGui(mc);
+            else
+                Battlegear.packetHandler.sendPacketToServer(new BattlegearGUIPacket(BattlegearGUIHandeler.mainID).generatePacket());
+        }
 		return inWindow;
 	}
 	
-	private boolean isInGui(GuiScreen currentScreen) {
+	public final boolean isInGui(GuiScreen currentScreen) {
 		return currentScreen.getClass()==getGUIClass();
 	}
 
 	@Override
-	public int getHoverState(boolean isMouseOver)
-    {
+	public int getHoverState(boolean isMouseOver){
 		if(!isInGui(Minecraft.getMinecraft().currentScreen)){
+            this.displayString = oldName;
 			return super.getHoverState(isMouseOver);
 		}
+        this.displayString = "Main";
 		return 0;
+    }
+
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY){
+        deltaX = mc.thePlayer.getActivePotionEffects().isEmpty() ? 0 : 120;
+        this.xPosition -= deltaX;
+        super.drawButton(mc, mouseX, mouseY);
+        this.xPosition += deltaX;
     }
 
 	protected abstract Class<? extends GuiScreen> getGUIClass();
--- battlegear mod src/minecraft/mods/battlegear2/gui/BattlegearGUIHandeler.java
+++ battlegear mod src/minecraft/mods/battlegear2/gui/BattlegearGUIHandeler.java
@@ -5,11 +5,13 @@ import mods.battlegear2.Battlegear;
 import mods.battlegear2.client.gui.BattleEquipGUI;
 import mods.battlegear2.client.gui.BattlegearSigilGUI;
 import mods.battlegear2.client.gui.GuiFlagDesigner;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ContainerPlayer;
 import net.minecraft.world.World;
 
 public class BattlegearGUIHandeler implements IGuiHandler {
-
+    public static final int mainID = 0;
     public static final int equipID = 1;
     public static final int sigilEditor = 2;
     public static final int flagEditor = 3;
@@ -18,9 +20,11 @@ public class BattlegearGUIHandeler implements IGuiHandler {
     public Object getServerGuiElement(int ID, EntityPlayer player, World world,
                                       int x, int y, int z) {
         switch (ID) {
+            case mainID:
+                return new ContainerPlayer(player.inventory, !world.isRemote, player);
             case equipID:
                 return new ContainerBattle(player.inventory, !world.isRemote, player);
-           case sigilEditor:
+            case sigilEditor:
                 return Battlegear.debug?new ContainerHeraldry(player.inventory, !world.isRemote, player):null;
             default:
                 return null;
@@ -32,6 +36,8 @@ public class BattlegearGUIHandeler implements IGuiHandler {
     public Object getClientGuiElement(int ID, EntityPlayer player, World world,
                                       int x, int y, int z) {
         switch (ID) {
+            case mainID:
+                return new GuiInventory(player);
             case equipID:
                 return new BattleEquipGUI(player, world.isRemote);
             case sigilEditor:
