5c65d3e7ae05b5a750119f864bd9865cc9b97fed
GotoLink <gotolinkminecraft@gmail.com>
2015-04-27 22:23:35 +0200

    Offhand block animation fix

--- battlegear mod src/minecraft/mods/battlegear2/client/utils/BattlegearRenderHelper.java
+++ battlegear mod src/minecraft/mods/battlegear2/client/utils/BattlegearRenderHelper.java
@@ -204,9 +204,9 @@ public final class BattlegearRenderHelper {
                         EnumAction action = offhandRender.getItemToRender().getItemUseAction();
 
                         if (action == EnumAction.block) {
-                            GL11.glTranslatef(-0.5F, 0.2F, 0.0F);
+                            GL11.glTranslatef(0.0F, 0.2F, 0.0F);
                             GL11.glRotatef(30.0F, 0.0F, 1.0F, 0.0F);
-                            GL11.glRotatef(-80.0F, 1.0F, 0.0F, 0.0F);
+                            GL11.glRotatef(30.0F, 1.0F, 0.0F, 0.0F);
                             GL11.glRotatef(60.0F, 0.0F, 1.0F, 0.0F);
                         } else if (action == EnumAction.bow) {
                             GL11.glRotatef(-18.0F, 0.0F, 0.0F, 1.0F);
