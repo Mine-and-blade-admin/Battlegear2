c9d0b6da56ef24eeee1701d9a1e334efc8094044
GotoLink <gotolinkminecraft@gmail.com>
2015-07-14 17:40:02 +0200

    Fixed some sheathed rendering

--- battlegear mod src/minecraft/mods/battlegear2/client/utils/BattlegearClientUtils.java
+++ battlegear mod src/minecraft/mods/battlegear2/client/utils/BattlegearClientUtils.java
@@ -3,6 +3,9 @@ package mods.battlegear2.client.utils;
 import mods.battlegear2.api.RenderPlayerEventChild;
 import mods.battlegear2.api.core.BattlegearUtils;
 import mods.battlegear2.api.core.InventoryPlayerBattle;
+import mods.battlegear2.api.quiver.IArrowContainer2;
+import mods.battlegear2.api.quiver.QuiverArrowRegistry;
+import mods.battlegear2.utils.BattlegearConfig;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemPotion;
@@ -10,6 +13,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
+
+import java.util.Arrays;
 
 public final class BattlegearClientUtils {
     /**
@@ -36,17 +41,30 @@ public final class BattlegearClientUtils {
         }
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onRenderOffhandItem(RenderPlayerEventChild.PreRenderSheathed preRender){
-        if(preRender.element!=null) {
-            if (preRender.element.getItem() instanceof ItemBucket || preRender.element.getItem() instanceof ItemPotion) {
-                flip(0.8F);
-            }
+    /**
+     * Flip the sheathed item
+     * @param stack
+     */
+    private void onRenderSheathedItem(ItemStack stack){
+        if (stack.getItem() instanceof ItemBucket || stack.getItem() instanceof ItemPotion || stack.getItem() instanceof IArrowContainer2) {
+            GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
         }
     }
 
-    public static void flip(float scale){
-        GL11.glScalef(scale, -scale, scale);
-        GL11.glTranslatef(0, -1, 0);
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onRenderOffhandItem(RenderPlayerEventChild.PreRenderPlayerElement preRender){
+        if(preRender.element!=null) {
+            if (preRender instanceof RenderPlayerEventChild.PreRenderSheathed) {
+                onRenderSheathedItem(preRender.element);
+            } else if(preRender.element.getItem() instanceof IArrowContainer2 && ((IArrowContainer2) preRender.element.getItem()).renderDefaultQuiverModel(preRender.element)){
+
+                if (Arrays.binarySearch(BattlegearConfig.disabledRenderers, "quiver") < 0) {
+                    ItemStack quiverStack = QuiverArrowRegistry.getArrowContainer(preRender.entityPlayer);
+                    if (preRender.element == quiverStack) {
+                        preRender.setCanceled(true);
+                    }
+                }
+            }
+        }
     }
 }
