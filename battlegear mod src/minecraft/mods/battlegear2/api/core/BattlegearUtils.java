cd097aa426f80f857c9e7223638413bb2d1088d1
GotoLink <gotolinkminecraft@gmail.com>
2015-07-20 20:10:32 +0200

    Catch all exceptions #191

--- battlegear mod src/minecraft/mods/battlegear2/api/core/BattlegearUtils.java
+++ battlegear mod src/minecraft/mods/battlegear2/api/core/BattlegearUtils.java
@@ -287,7 +287,7 @@ public class BattlegearUtils {
             try {
                 c.getDeclaredMethod(itemBlackListMethodNames[i], itemBlackListMethodParams[i]);
                 return true;
-            } catch (NoSuchMethodException ignored) {
+            } catch (Exception ignored) {
             }
         }
         return false;
